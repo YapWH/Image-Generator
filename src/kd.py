8e371b9e61fe67715c06a61f21c0441438efdb8a
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2024-06-21 22:30:42 +0800

    Initial upload

---
+++ src/kd.py
@@ -0,0 +1,217 @@
+import os
+import pickle
+import logging
+import numpy as np
+from tqdm import tqdm
+from time import time
+import matplotlib.pyplot as plt
+
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+from network import ContextUnet
+from utils import CustomDataset, perturb_input, denoise_add_noise, plot_sample, plot_grid, knowledge_distillation_loss, get_trainable_params, set_logger
+
+################################################################################
+
+def train(nn_model, n_epoch, optimizer, lr, dataloader, device, ab_t, criterion, timesteps=500) -> list:
+    nn_model.train()
+    loss_hist = []
+
+    with tqdm(range(n_epoch), desc="Training") as pbar:
+        for ep in pbar:
+            optimizer.param_groups[0]['lr'] = lr * (1-ep/n_epoch) # linearly decay learning rate (lr scheduler)
+            total_loss = 0
+            for x, _ in dataloader:   # x: images
+                optimizer.zero_grad()
+                x = x.to(device)
+                
+                # perturb data
+                noise = torch.randn_like(x)
+                t = torch.randint(1, timesteps + 1, (x.shape[0],)).to(device) 
+                x_pert = perturb_input(x, t, noise, ab_t)
+                
+                # use network to recover noise
+                pred_noise = nn_model(x_pert, t / timesteps)
+                loss = criterion(pred_noise, noise)
+                loss.backward()
+                total_loss += loss.item()
+                
+                optimizer.step()
+
+            pbar.set_postfix({'loss': total_loss, 'lr': optimizer.param_groups[0]['lr']})
+            loss_hist.append(total_loss)
+    
+    return loss_hist
+
+def trainStudent(teacher_model, student_model, n_epoch, optimizer, lr, dataloader, device, ab_t, timesteps=500) -> list:
+    teacher_model.eval()
+    student_model.train()
+    loss_hist = []
+
+    with tqdm(range(n_epoch), desc="Training") as pbar:
+        for ep in pbar:
+            optimizer.param_groups[0]['lr'] = lr * (1-ep/n_epoch) # linearly decay learning rate (lr scheduler)
+            total_loss = 0
+            for x, _ in dataloader:   # x: images
+                optimizer.zero_grad()
+                x = x.to(device)
+
+                # perturb data
+                noise = torch.randn_like(x)
+                t = torch.randint(1, timesteps + 1, (x.shape[0],)).to(device)
+                x_pert = perturb_input(x, t, noise, ab_t)
+
+                # use network to recover noise
+                teacher_noise = teacher_model(x_pert, t / timesteps)
+                student_noise = student_model(x_pert, t / timesteps)
+                loss = knowledge_distillation_loss(teacher_noise, student_noise, noise)
+                loss.backward()
+                total_loss += loss.item()
+
+                optimizer.step()
+
+            pbar.set_postfix({'loss': total_loss, 'lr': optimizer.param_groups[0]['lr']})
+            loss_hist.append(total_loss)
+    
+    return loss_hist
+
+################################################################################
+
+@torch.no_grad()
+def sample_ddpm(nn_model, n_sample, height, timesteps, device, ab_t, a_t, b_t, save_rate=20):
+    nn_model.eval()
+    # x_T ~ N(0, 1), sample initial noise
+    samples = torch.randn(n_sample, 3, height, height).to(device)  
+
+    # array to keep track of generated steps for plotting
+    intermediate = [] 
+    for i in range(timesteps, 0, -1):
+        print(f'sampling timestep {i:3d}', end='\r')
+
+        # reshape time tensor
+        t = torch.tensor([i / timesteps])[:, None, None, None].to(device)
+
+        # sample some random noise to inject back in. For i = 1, don't add back in noise
+        z = torch.randn_like(samples) if i > 1 else 0
+
+        eps = nn_model(samples, t)    # predict noise e_(x_t,t)
+        samples = denoise_add_noise(samples, i, eps, ab_t, a_t, b_t, z)
+        if i % save_rate ==0 or i==timesteps or i<8:
+            intermediate.append(samples.detach().cpu().numpy())
+
+    intermediate = np.stack(intermediate)
+    return samples, intermediate
+
+################################################################################
+
+def main():
+    set_logger(f"{time()}.log")
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    # diffusion hyperparameters
+    timesteps = 1000
+    beta1 = 1e-4
+    beta2 = 0.02
+
+    # construct DDPM noise scheduler
+    b_t = (beta2 - beta1) * torch.linspace(0, 1, timesteps + 1, device=device) + beta1
+    a_t = 1 - b_t
+    ab_t = torch.cumsum(a_t.log(), dim=0).exp()    
+    ab_t[0] = 1
+
+    # network hyperparameters
+    device = torch.device("cuda:0" if torch.cuda.is_available() else torch.device('cpu'))
+    logging.info(f"Using device: {device}")
+    n_feat_teacher = 64 # 64 hidden dimension feature
+    n_feat_student = 32 # 32 hidden dimension feature
+    n_cfeat = 5 # context vector is of size 5
+    height = 16 # 16x16 image
+    save_dir = './results/'
+    if not os.path.exists(save_dir): os.makedirs(save_dir)
+
+    # training hyperparameters
+    batch_size = 100
+    n_epoch = 50
+    lrate=1e-3
+
+    # load dataset
+    logging.info("Loading dataset...")
+    dataset = CustomDataset("/data/sprites.npy", "/data/sprites_labels.npy", transform, null_context=False)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1)
+
+    # initialize teacher model
+    teacher_model = ContextUnet(in_channels=3, n_feat=n_feat_teacher, n_cfeat=n_cfeat, height=height).to(device)
+    optimizer = torch.optim.Adam(teacher_model.parameters(), lr=lrate)
+    criterion = torch.nn.MSELoss()
+    logging.info(f"Number of trainable parameters in teacher model: {get_trainable_params(teacher_model)}")
+
+    # train the model
+    teacher_loss_hist = train(teacher_model, n_epoch, optimizer, lrate, dataloader, device, ab_t, criterion, timesteps)
+
+    # save the model
+    if not os.path.exists('./models'): os.makedirs('./models')
+    torch.save(teacher_model.state_dict(), './models/model.pth')
+    pickle.dump(ab_t, open('./models/ab_t.pkl', 'wb'))
+    pickle.dump(a_t, open('./models/a_t.pkl', 'wb'))
+    pickle.dump(b_t, open('./models/b_t.pkl', 'wb'))
+    logging.info("Teacher Model saved.")
+
+    # initialize student model
+    student_model = ContextUnet(in_channels=3, n_feat=n_feat_student, n_cfeat=n_cfeat, height=height).to(device)
+    student_optimizer = torch.optim.Adam(student_model.parameters(), lr=lrate)
+    logging.info(f"Number of trainable parameters in student model: {get_trainable_params(student_model)}")
+
+    # train the student model
+    student_loss_hist = trainStudent(teacher_model, student_model, n_epoch, student_optimizer, lrate, dataloader, device, ab_t, timesteps)
+
+    # save the student model
+    torch.save(teacher_model.state_dict(), './models/student_model.pth')
+    logging.info("Student Model saved.")
+
+    # Plot the training loss
+    plt.figure(figsize=(10, 5))
+    plt.plot(teacher_loss_hist, label='teacher')
+    plt.plot(student_loss_hist, label='student')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training Loss')
+    plt.legend()
+    plt.savefig(os.path.join(save_dir, 'loss.png'))
+    plt.close()
+    logging.info("Training Loss plot saved.")
+
+################################################################################
+
+def test():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else torch.device('cpu'))
+    nn_model = ContextUnet(in_channels=3, n_feat=64, n_cfeat=5, height=16).to(device)
+    nn_model.load_state_dict(torch.load('./models/model.pth'))
+    height = 16
+    timesteps = 500
+    save_dir = './results/'
+    if not os.path.exists(save_dir): os.makedirs(save_dir)
+    
+    # load diffusion hyperparameters
+    ab_t = pickle.load(open('./models/ab_t.pkl', 'rb'))
+    a_t = pickle.load(open('./models/a_t.pkl', 'rb'))
+    b_t = pickle.load(open('./models/b_t.pkl', 'rb'))
+
+    # sample from the model
+    n_sample = 32
+    samples, intermediate = sample_ddpm(nn_model, n_sample, height, timesteps, device, ab_t, a_t, b_t, save_rate=20)
+    animation_ddpm = plot_sample(intermediate, 32, 4, save_dir, f"{time()}", None, save=True)
+    plot_grid(samples, n_sample, 4, save_dir, timesteps)
+
+################################################################################
+
+
+if __name__ == "__main__":
+    main()
+
+    test()
---
+++ src/utils.py
@@ -0,0 +1,185 @@
+import numpy as np
+import torch
+from torchvision.utils import make_grid, save_image
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.animation import PillowWriter
+from torch.utils.data import Dataset
+import torch.nn.functional as F
+import os
+import logging
+
+################################################################################
+
+def unorm(x):
+    # unity norm. results in range of [0,1]
+    # assume x (h,w,3)
+    xmax = x.max((0,1))
+    xmin = x.min((0,1))
+    return(x - xmin)/(xmax - xmin)
+
+################################################################################
+
+def norm_all(store, n_t, n_s):
+    # runs unity norm on all timesteps of all samples
+    nstore = np.zeros_like(store)
+    for t in range(n_t):
+        for s in range(n_s):
+            nstore[t,s] = unorm(store[t,s])
+    return nstore
+
+################################################################################
+
+def norm_torch(x_all):
+    # runs unity norm on all timesteps of all samples
+    # input is (n_samples, 3,h,w), the torch image format
+    x = x_all.cpu().numpy()
+    xmax = x.max((2,3))
+    xmin = x.min((2,3))
+    xmax = np.expand_dims(xmax,(2,3)) 
+    xmin = np.expand_dims(xmin,(2,3))
+    nstore = (x - xmin)/(xmax - xmin)
+    return torch.from_numpy(nstore)
+
+################################################################################
+
+def gen_tst_context(n_cfeat):
+    """
+    Generate test context vectors
+    """
+    vec = torch.tensor([
+    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0],      # human, non-human, food, spell, side-facing
+    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0],      # human, non-human, food, spell, side-facing
+    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0],      # human, non-human, food, spell, side-facing
+    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0],      # human, non-human, food, spell, side-facing
+    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0],      # human, non-human, food, spell, side-facing
+    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0]]      # human, non-human, food, spell, side-facing
+    )
+    return len(vec), vec
+
+################################################################################
+
+def plot_grid(x, n_sample, n_rows, save_dir, w):
+    # x:(n_sample, 3, h, w)
+    ncols = n_sample//n_rows
+    grid = make_grid(norm_torch(x), nrow=ncols)  # curiously, nrow is number of columns.. or number of items in the row.
+    save_image(grid, save_dir + f"run_image_w{w}.png")
+    print('saved image at ' + save_dir + f"run_image_w{w}.png")
+    return grid
+
+################################################################################
+
+def plot_sample(x_gen_store, n_sample, nrows, save_dir, fn,  w, save=False):
+    ncols = n_sample//nrows
+    sx_gen_store = np.moveaxis(x_gen_store,2,4)                               # change to Numpy image format (h,w,channels) vs (channels,h,w)
+    nsx_gen_store = norm_all(sx_gen_store, sx_gen_store.shape[0], n_sample)   # unity norm to put in range [0,1] for np.imshow
+    
+    # create gif of images evolving over time, based on x_gen_store
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True,figsize=(ncols,nrows))
+
+    def animate_diff(i, store):
+        print(f'gif animating frame {i} of {store.shape[0]}', end='\r')
+        plots = []
+        for row in range(nrows):
+            for col in range(ncols):
+                axs[row, col].clear()
+                axs[row, col].set_xticks([])
+                axs[row, col].set_yticks([])
+                plots.append(axs[row, col].imshow(store[i,(row*ncols)+col]))
+        return plots
+    
+    ani = FuncAnimation(fig, animate_diff, fargs=[nsx_gen_store],  interval=200, blit=False, repeat=True, frames=nsx_gen_store.shape[0]) 
+    plt.close()
+
+    if save:
+        ani.save(save_dir + f"{fn}_w{w}.gif", dpi=100, writer=PillowWriter(fps=5))
+        print('saved gif at ' + save_dir + f"{fn}_w{w}.gif")
+
+    return ani
+
+################################################################################
+
+class CustomDataset(Dataset):
+    def __init__(self, sfilename, lfilename, transform, null_context=False):
+        self.sprites = np.load(sfilename)
+        self.slabels = np.load(lfilename)
+        print(f"sprite shape: {self.sprites.shape}")
+        print(f"labels shape: {self.slabels.shape}")
+        self.transform = transform
+        self.null_context = null_context
+        self.sprites_shape = self.sprites.shape
+        self.slabel_shape = self.slabels.shape
+                
+    # Return the number of images in the dataset
+    def __len__(self):
+        return len(self.sprites)
+    
+    # Get the image and label at a given index
+    def __getitem__(self, idx):
+        # Return the image and label as a tuple
+        if self.transform:
+            image = self.transform(self.sprites[idx])
+            if self.null_context:
+                label = torch.tensor(0).to(torch.int64)
+            else:
+                label = torch.tensor(self.slabels[idx]).to(torch.int64)
+        return (image, label)
+
+    def getshapes(self):
+        # return shapes of data and labels
+        return self.sprites_shape, self.slabel_shape
+
+################################################################################
+
+# helper function: perturbs an image to a specified noise level
+def perturb_input(x, t, noise, ab_t):
+    return ab_t.sqrt()[t, None, None, None] * x + (1 - ab_t[t, None, None, None]) * noise
+
+################################################################################
+
+# helper function; removes the predicted noise (but adds some noise back in to avoid collapse)
+def denoise_add_noise(x, t, pred_noise, ab_t, a_t, b_t, z=None):
+    if z is None:
+        z = torch.randn_like(x)
+    noise = b_t.sqrt()[t] * z
+    mean = (x - pred_noise * ((1 - a_t[t]) / (1 - ab_t[t]).sqrt())) / a_t[t].sqrt()
+    return mean + noise
+
+################################################################################
+
+def knowledge_distillation_loss(teacher_logits, student_logits, true_labels, alpha=0.5, beta=0.5):
+    soft_target_loss = F.kl_div(F.log_softmax(student_logits, dim=1), F.softmax(teacher_logits, dim=1), reduction='batchmean')
+    mse_loss = F.mse_loss(student_logits, F.one_hot(true_labels, num_classes=student_logits.size(1)).float(), reduction='mean')
+    distillation_loss = alpha * soft_target_loss + beta * mse_loss
+    
+    return distillation_loss
+
+################################################################################
+
+def get_trainable_params(model):
+    total_params = 0
+    for param in model.parameters():
+        if param.requires_grad:
+            total_params += param.numel()
+    return total_params
+
+################################################################################
+
+def set_logger(log_path):
+	# remove the log with same name
+    if os.path.exists(log_path) is True:
+        os.remove(log_path)
+    # Initialize log
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+        logger.addHandler(file_handler)
+
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
