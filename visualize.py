8f5214f3b496464aa464c5ccfd4908a6bc570dd0
Cao Rui <107180053+HawkingC@users.noreply.github.com>
2024-07-01 15:36:33 +0800

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -1,14 +1,23 @@
 import numpy as np
 import gradio as gr
 import random
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
+
 a = random.randint(0, 9)
 b = random.randint(0, 9)
 c = random.randint(0, 9)
 d = random.randint(0, 9)
 
+image_path = 'Knowledge Distillation.png' 
+image = Image.open(image_path)
+
 def test(A):
 	if A == f"{a,b,c,d}":
-		return "Congratulations! You got the right validation code!"
+		img_dir = 'E:\AML\Image-Generator'
+		imgs_List=[ os.path.join(img_dir,name) for name in sorted(os.listdir(img_dir)) if  name.endswith(('.png','.jpg','.webp','.tif','.jpeg'))]
+		return imgs_List
 	#test()
 	else:
 		return "Sorry! You got the wrong validation code! Please try again!"
@@ -19,7 +28,7 @@ demo = gr.Interface(
     [
         gr.Radio([f"{a,b,c,d}", "(6,6,5,4)", "(7,4,6,4)"], label="Validation", info=f"Please choose the right validation code before you start!    Prompt  :  {a,b,c,d}"),
     ],
-    "text",
+    outputs=gr.Gallery(label="Diffusion"),
 	title="PixeR",
     description="Are you worried about lacking of materials for designing a pixel styple game? Here comes a solution! Our project will generate a group of 16 x 16 pages with total number of 32. Feel free to use our tool to generate the materials you need!",
 )
